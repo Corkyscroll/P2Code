e3cb1dfe34529ac1d18b9adfd8b1123cfc2f4bd7
Corkyscroll <Dummemartini@hotmail.com>
2016-04-28 14:36:46 +0200

    Added files via upload

--- P2Code/src/ShoppingList/Execute.java
+++ P2Code/src/ShoppingList/Execute.java
@@ -1,17 +1,119 @@
 package ShoppingList;
 
+import java.awt.GridLayout;
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import javax.swing.*;
 
-public class Execute {
-    public static void main(String[] args) {
-         ArrayList<String> names = new ArrayList<String>();
-         names.add("potato");
-         names.add("milk");
-         names.add("eggs");
-         names.add("Shoes");
+@SuppressWarnings("serial")
+public class Execute extends JFrame implements ActionListener {
+    private JButton overviewBTN;
+    private JButton xpenseBTN;
+    private JButton shpnlistBTN;
+    public Execute() {
+        //Set up the main window frame
+        setTitle("Execute");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 700);
+        
+        //Initialize the components
+        overviewBTN = new JButton("Overview");
+        xpenseBTN   = new JButton("New Expense");
+        shpnlistBTN = new JButton("Shopping List");
+        
+        //Set up the layout of the components
+        GridLayout layout = new GridLayout(0, 1);
+        getContentPane().setLayout(layout);
+        
+        //Add the components to the window
+        add(overviewBTN);
+        add(xpenseBTN);
+        add(shpnlistBTN);
+        
+        //Register ActionListeners for the buttons
+        overviewBTN.addActionListener(this);
+        xpenseBTN.addActionListener(this);
+        shpnlistBTN.addActionListener(this);
+
+        pack();
+        setVisible(true);
+    }
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+           JButton btn = (JButton)  e.getSource();
+           if (btn == overviewBTN)
+           {
+               System.out.println("You pressed: " + btn.getText());
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        new Overview();
+                    }
+                });
+           }
+           else if (btn == xpenseBTN)
+            {
+               System.out.println("You pressed: " + btn.getText());
+            }
+           else if (btn == shpnlistBTN)
+           {
+               System.out.println("You pressed: " + btn.getText());
+           }
+        }
     
-    //     kartoffel p = new kartoffel(names);
-    //     Category q = new Category();
+//    private class XpenseButtonListener implements ActionListener {
+//        @Override
+//        public void actionPerformed(ActionEvent e)
+//        {
+//            SwingUtilities.invokeLater(new Runnable() {
+//                public void run() {
+//                    new Overview();
+//                }
+//            });
+//        }
+//    }       
+    
+    public static void main(String[] args) {
+        // Attributes
+         ArrayList<String> names          = new ArrayList<String>();
+         ArrayList<String> loadedFile     = new ArrayList<String>();
+         ArrayList<String> categoryList = new ArrayList<String>();
+         ArrayList<String> itemList       = new ArrayList<String>();
+         
+         File file           = new File("test1.txt");
+         File categoryFile = new File("categories.txt");
+         File itemFile       = new File("items.txt");
+         
+//         String hi = "I'm not a category :i";
+         
+         names.add("Potato");
+         names.add("Onion");
+         names.add("Rice");
+         names.add("Shoes");
+         
+         
+         try {
+              new Save(names);
+              new Save(categoryList, categoryFile);
+              new Category();
+              new Item();
+              new Load(file, loadedFile);
+              new Load(categoryFile, categoryList);
+              new Load(itemFile, itemList);
+              
+              Item.dropDown = categoryList.toArray(new String[categoryList.size()]);
+             
+         } catch (FileNotFoundException e) {
+             e.printStackTrace();
+         }
+         
+         SwingUtilities.invokeLater(new Runnable() {
+             public void run() {
+                 new Execute();
+             }
+         });
          
     }
 }
--- P2Code/src/ShoppingList/Item.java
+++ P2Code/src/ShoppingList/Item.java
@@ -1,69 +1,119 @@
 package ShoppingList;
 
-public class Item {
-	//Instance variables
-	private String itemName;
-	private String category;
-	private Double price;
-	private Boolean isOnSale;
-	private Double discountPrice;
-	
-	//Constructors
-	public Item(){
-		setItemName(null);
-		setCategory(null);
-		setPrice(null);
-		setIsOnSale(null);
-		setDiscountPrice(null);
-		
-	}
-	
-	public Item(String itemName, String category, Double price, Boolean isOnSale, Double discountPrice){
-		setItemName(itemName);
-		setCategory(category);
-		setPrice(price);
-		setIsOnSale(isOnSale);
-		setDiscountPrice(discountPrice);
-			
-	}
-	
-	//Getters and setters
-	public String getItemName()
-	{
-		return itemName;
-	}
-	
-	public void setItemName(String item)
-	{
-		this.itemName = item;
-	}
-	
-	public Double getPrice()
-	{
-		return price;
-	}
-	
-	public void setPrice(Double price)
-	{
-		this.price = price;
-	}
-	
-	public Boolean getIsOnSale()
-	{
-		return isOnSale;
-	}
-	public void setIsOnSale (Boolean isOnSale)
-	{
-		this.isOnSale = isOnSale;
-	}
-	
-	public Double getDiscountPrice()
-	{
-		return discountPrice;
-	}
-	
-	public void setDiscountPrice(Double discountPrice)
-	{
-		this.discountPrice = discountPrice;
-	}
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+//import javax.swing.*;
+
+public class Item {    
+    //Instance variables
+    private ArrayList<String> itemList = new ArrayList<String>();
+    private ArrayList<String> categoryList = new ArrayList<String>();
+    private String itemName;
+    private String category;
+    private Double price;
+    private Boolean isOnSale;
+    private Double discountPrice;
+    public static String[] dropDown;
+//    private static JComboBox<String> categoryChooser = new JComboBox<String>(dropDown);;
+    File itemFile = new File("items.txt");
+    File categoryFile = new File("categories.txt");
+    
+    
+    //Constructors
+    public Item() throws FileNotFoundException {
+        loadCategoryList();
+        checkItemList();
+        
+        setItemName(null);
+        setCategory(null);
+        setPrice(null);
+        setIsOnSale(null);
+        setDiscountPrice(null);
+        
+        saveItemList();
+    }
+    
+    public Item(String itemName, String category, Double price, Boolean isOnSale, Double discountPrice) throws FileNotFoundException {
+        setItemName(itemName);
+        setCategory(category);
+        setPrice(price);
+        setIsOnSale(isOnSale);
+        setDiscountPrice(discountPrice);
+        
+        saveItemList();
+    }
+    
+    public Item(String itemName) {
+        setItemName(itemName);
+        
+        saveItemList();
+    }
+    
+    //Getters and setters
+    public String getItemName()
+    {
+        return itemName;
+    }
+    public void setItemName(String item)
+    {
+        this.itemName = item;
+    }
+    
+    public String getCategory()
+    {
+        return category;
+    }
+    public void setCategory(String category)
+    {
+        this.category = category;
+        // hent index fra categoryList dropdown menu
+    }
+    
+    public Double getPrice()
+    {
+        return price;
+    }
+    public void setPrice(Double price)
+    {
+        this.price = price;
+    }
+    
+    public Boolean getIsOnSale()
+    {
+        return isOnSale;
+    }
+    public void setIsOnSale (Boolean isOnSale)
+    {
+        this.isOnSale = isOnSale;
+    }
+    
+    public Double getDiscountPrice()
+    {
+        return discountPrice;
+    }
+    public void setDiscountPrice(Double discountPrice)
+    {
+        this.discountPrice = discountPrice;
+    }
+    
+    public void checkItemList() throws FileNotFoundException {
+        try {
+            new Load(itemFile, itemList);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void loadCategoryList() throws FileNotFoundException {
+        try {
+            new Load(categoryFile, categoryList);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void saveItemList() {
+        new Save(itemList, itemFile);
+    }
 }
