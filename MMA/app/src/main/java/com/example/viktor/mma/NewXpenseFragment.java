653349a8138e0281af9e8072f31dfa7cfb88a25d
Corkyscroll <Dummemartini@hotmail.com>
2016-05-04 23:48:01 +0200

    Last commit

--- MMA/app/src/main/java/com/example/viktor/mma/NewXpenseFragment.java
+++ MMA/app/src/main/java/com/example/viktor/mma/NewXpenseFragment.java
@@ -49,10 +49,10 @@ public class NewXpenseFragment extends Fragment {
                             Toast.makeText(getActivity(), "Please enter an item name and a price", Toast.LENGTH_LONG).show();
                         }
                         else {
-                            if (switchstatus.isChecked()){
-                                SaveClass.setunplanned(Double.parseDouble(itemPrice.getText().toString()));
-
-                            }
+//                            if (switchstatus.isChecked()){
+//                                SaveClass.setunplanned(Double.parseDouble(itemPrice.getText().toString()));
+//
+//                            }
                             SaveClass.setCamefromnewexpense(true);
                             SaveClass.SetShoppingListContainer(itemName.getText().toString());
                             SaveClass.CountUp();
